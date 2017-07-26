45310e54d4fbe99139b81b1eca2b3547173415de
Joe Spedale <jspedale@gmail.com>
2017-07-25 22:20:42 -0400

    Added Procfile for heroku deployment and trimmed extra characters from the beginning of IP address field.

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/', (req, res) => {
   const lang = req.headers['accept-language'];
   const os = req.headers['user-agent'];
+  const ip = req.ip;
 
+  const ipaddress = ip.substring(7, ip.length - 1);
   const language = lang.substring(0, lang.indexOf(','));
   const software = os.substring(os.indexOf('(') + 1, os.indexOf(')'));
 
